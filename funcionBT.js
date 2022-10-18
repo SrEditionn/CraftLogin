6232a432f6735d6f8b3e9a4fabe63b334f58760b
SrEdition <71359788+SrEditionn@users.noreply.github.com>
2022-10-17 22:33:04 -0300

    Update funcionBT.js

--- funcionBT.js
+++ funcionBT.js
@@ -2,7 +2,7 @@ function logar(){
     var login = document.getElementById('log').value;
     var senha = document.getElementById('pass').value;
 
-    if(login =="SrEdition" && senha =="1234") {
+    if(login =="SrEdition" && senha =="244466666") {
     location.href= "menu.html"
     }
     else {
