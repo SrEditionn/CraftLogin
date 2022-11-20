5ddc5558565a3fbc183a13d6c92ac7d7f9ed54f6
SrEdition <71359788+SrEditionn@users.noreply.github.com>
2022-11-20 18:25:10 -0300

    Add files via upload

--- funcionBT.js
+++ funcionBT.js
@@ -2,7 +2,7 @@ function logar(){
     var login = document.getElementById('log').value;
     var senha = document.getElementById('pass').value;
 
-    if(login =="SrEdition" && senha =="244466666") {
+    if(login =="SrEdition" && senha =="231427") {
     location.href= "menu.html"
     }
     else {
